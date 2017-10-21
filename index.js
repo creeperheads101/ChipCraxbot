d5ab98a208388ee396a0e23f8f2bdb9a75a35eb6
creeperheads101 <32970555+creeperheads101@users.noreply.github.com>
2017-10-20 18:38:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const Discord = require('discord.js');
 const bot1 = new Discord.Client();
 
 bot1.on('message', (message) => {
-    if(message.content == '!whitelist testkey') {
-        message.author.sendMessage("Whitelisted")
+    if(message.content == '!whitelist') {
+        message.author.sendMessage("https://cdn.discordapp.com/attachments/368956506089062402/371093886082351105/Whitelister.zip")
     }
 });
+
+bot1.on('message', (message) => {
+    if(message.content == '!whitelist nge78ghb3') {
+        message.author.sendMessage("")
+    }
+});
+
+
 
 
 bot1.login(process.env.BOT_TOKEN);
