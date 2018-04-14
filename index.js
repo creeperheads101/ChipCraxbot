5fb29307d216989aad5a1e6496cd376873ce7682
creeperheads101 <32970555+creeperheads101@users.noreply.github.com>
2018-04-14 11:33:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -146,13 +146,7 @@ client.on('message', message => {
 
 
     };
-    if (msg.startsWith(prefix+'PING')) { 
 
-        
-        message.channel.send("@everyone");
-
-
-    };
     if (msg.startsWith(prefix+'HELP')) { 
         message.channel.send({embed: {
             color: 3447003,
